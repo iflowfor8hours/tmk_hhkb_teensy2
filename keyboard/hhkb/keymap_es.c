7ec2c2ad88fbd8cfd8b034d6aaf1fb0e4310be2a
matt urbanski <matt.urbanski@digital.cabinet-office.gov.uk>
2015-08-26 11:40:14 +0100

    Getting closer to perfect

--- keyboard/hhkb/keymap_es.c
+++ keyboard/hhkb/keymap_es.c
@@ -16,14 +16,14 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------------------------|
      * |Shift      |   Z|   X|   C|   V|   B|   N|   M|   ,|   .|   /|Shift    |Fn0  |
      * |-----------------------------------------------------------------------------|
-     *       |Gui  |Alt    |         SpaceFN         |Alt    |Gui  |
+     *       |Fn0  |Alt    |         SpaceFN         |Alt    |Fn0  |
      *       `-----------------------------------------------------'
      */
-    KEYMAP(FN2, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, _VOLDOWN, _VOLUP,  \
-           TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC, FN3,              \
+    KEYMAP(FN2, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS, GRV,  \
+           TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,              \
            LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,                    \
            LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN0,                    \
-                LGUI,LALT,          FN1,                RALT,RGUI),
+                FN0,LALT,          FN1,                RALT,FN0),
 
     /* Layer 1: HHKB mode (HHKB Fn)
      * ,-----------------------------------------------------------.
@@ -31,7 +31,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |Caps |   |   |   |   |   |   |   |Psc|Slk|Pus|Up |   |Backs|
      * |-----------------------------------------------------------|
-     * |      |VoD|VoU|Mut|   |   |  *|  /|Hom|PgU|Lef|Rig|Enter   |
+     * |      |VoD|VoU|Mut|   |   |  *|  /|Hom|PgU|Lef|   |Enter   |
      * |-----------------------------------------------------------|
      * |        |   |   |   |   |   |  +|  -|End|PgD|Dow|      |   |
      * `-----------------------------------------------------------'
@@ -40,7 +40,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */ 
     KEYMAP(POWER, SLEP,  WAKE,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10,TRNS, F11, TRNS, TRNS,  \
            CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS, BSLS,                \
-           TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,PENT,                      \
+           TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,LEFT,DOWN,  UP,RGHT,RGHT,TRNS,PENT,                      \
            TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,TRNS,                      \
                 TRNS,TRNS,          SPC,                TRNS,TRNS),
 
